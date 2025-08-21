982b607d694a8281b98e10a1e0bda7d9a7926998
Riad <riadk910@gmail.com>
2025-08-21 14:18:45 +0600

    paths added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import DashBoard from "./pages/DashBoard";
 import WriteArticle from "./pages/WriteArticle";
 import BlogTitles from "./pages/BlogTitles";
 import Layout from "./pages/Layout";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import Community from "./pages/Community";
 
 const App = () => {
   return (
@@ -15,6 +19,10 @@ const App = () => {
           <Route index element={<DashBoard />} />
           <Route path="write-article" element={<WriteArticle />} />
           <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="community" element={<Community />} />
         </Route>
       </Routes>
     </div>
