192cae771f6a6359b55a6e8f81559f1baa6091a8
Riad <riadk910@gmail.com>
2025-08-24 12:20:36 +0600

    Hero component created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-screen w-full bg-[url('/gradientBackground.png')] bg-cover bg-center">
+      Hero
+    </div>
+  );
+};
+
+export default Hero;
