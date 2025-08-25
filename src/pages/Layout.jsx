fa604e28cd06bb678d7b5d1f9cbf604643491017
Riad <riadk910@gmail.com>
2025-08-25 14:49:42 +0600

    Sidebar prop passed

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { Menu, X } from "lucide-react";
+import Sidebar from "../components/Sidebar";
+import { SignIn, SignInButton, useUser } from "@clerk/clerk-react";
 
 const Layout = () => {
   const navigate = useNavigate();
   const [sidebar, setSidebar] = useState(false);
-  return (
+  const { user } = useUser();
+  return user ? (
     <div className="flex flex-col items-start justify-start h-screen">
       <nav>
         <img src={assets.logo} alt="logo" onClick={() => navigate("/")} />
@@ -22,7 +25,16 @@ const Layout = () => {
           />
         )}
       </nav>
-      <Outlet />
+      <div className="flex-1 w-full flex h-[calc(100vh-64px)]">
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
+        <div className="flex-1 bg-[#F4F7FB]">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
     </div>
   );
 };
