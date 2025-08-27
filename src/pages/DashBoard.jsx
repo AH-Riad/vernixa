c86010952115e1af2fa8c818d4bfb317b50f0fdf
Riad <riadk910@gmail.com>
2025-08-27 11:51:11 +0600

    Updated Dashboard page

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { dummyCreationData } from "../assets/assets";
-import { Sparkles } from "lucide-react";
+import { Gem, Sparkles } from "lucide-react";
 import { Protect } from "@clerk/clerk-react";
 
 const DashBoard = () => {
@@ -35,7 +35,13 @@ const DashBoard = () => {
               </Protect>
             </h2>
           </div>
+          <div className=" w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center">
+            <Gem className="w-5 text-white" />
+          </div>
         </div>
+      </div>
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
       </div>
     </div>
   );
