912ee403594622d9124c6184fa1524c458b08015
Riad <riadk910@gmail.com>
2025-08-31 11:12:23 +0600

    BlogTitle page addded

--- src/pages/BlogTitles.jsx
+++ src/pages/BlogTitles.jsx
@@ -1,9 +1,86 @@
-import React from "react";
+import { Hash, Sparkle } from "lucide-react";
+import React, { useState } from "react";
 
 const BlogTitles = () => {
+  const blogCategories = [
+    "General",
+    "Technology",
+    "Business",
+    "Health",
+    "Lifestyle",
+    "Education",
+    "Travel",
+    "Food",
+  ];
+  const [selectedCategory, setSelectedCategory] = useState("General");
+  const [input, setInput] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>BlogTitles</h1>
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Left col */}
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkle className="w-6 text-[#8E37EB]" />
+          <h1 className="text-xl font-semibold">AI Title Generator</h1>
+        </div>
+
+        {/* Topic Input */}
+        <p className="mt-6 text-sm font-medium">Keyword</p>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          type="text"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="The future of artificial intelligence is..."
+          required
+        />
+
+        {/* Length Options */}
+        <p className="mt-4 text-sm font-medium">Category</p>
+        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
+          {blogCategories.map((item, index) => (
+            <span
+              onClick={() => setSelectedCategory(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer transition-colors ${
+                selectedCategory === item
+                  ? "bg-blue-50 text-purple-700 border-purple-700"
+                  : "text-gray-500 border-gray-300 hover:bg-gray-100"
+              }`}
+              key={index}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <br />
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Hash className="w-5" />
+          Generate title
+        </button>
+      </form>
+
+      {/* Right col */}
+
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg  flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+        <div className="flex items-center gap-3">
+          <Hash className="w-5 h-5 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Generated Article</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Hash className="w-9 h-9 text-[#8E37EB]" />
+            <p className="text-slate-500">
+              Enter a topic and click ❝Generate title❞ to get strated
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
