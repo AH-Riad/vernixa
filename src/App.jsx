0ecc3fe41f05fe9c347a5093f594de82b1bf77d8
Riad <riadk910@gmail.com>
2025-09-01 10:45:27 +0600

    Review resume path added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import GenerateImages from "./pages/GenerateImages";
 import RemoveBackground from "./pages/RemoveBackground";
 import RemoveObject from "./pages/RemoveObject";
 import Community from "./pages/Community";
+import ReviewResume from "./pages/ReviewResume";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
           <Route path="generate-images" element={<GenerateImages />} />
           <Route path="remove-background" element={<RemoveBackground />} />
           <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
           <Route path="community" element={<Community />} />
         </Route>
       </Routes>
