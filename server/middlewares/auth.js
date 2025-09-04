500b673b86548d84738c63c918d85774269ca549
Riad <riadk910@gmail.com>
2025-09-04 13:23:07 +0600

    auth added

---
+++ server/middlewares/auth.js
@@ -0,0 +1,25 @@
+//middleware to check userId and premium plan
+import { clerkClient } from "@clerk/express";
+import { preinit } from "react-dom";
+
+export const auth = async (req, res, next) => {
+  try {
+    const { userId, has } = await req.auth();
+    const hasPremiumPlan = await has({ plan: "premium" });
+    const user = await clerkClient.users.getUser(userId);
+
+    if (!hasPremiumPlan && user.privateMedata.free_usage) {
+      req.free_usage = user.privateMedata.free_usage;
+    } else {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMedata: {
+          free_usage: 0,
+        },
+      });
+      req.free_usage = 0;
+    }
+    req.plan = hasPremiumPlan ? "premium" : "free";
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
