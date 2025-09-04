7e8e5a698f40054a66c9508f89847bc9f5ea806e
Riad <riadk910@gmail.com>
2025-09-04 20:22:03 +0600

    aiRouter added

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { auth } from "../middlewares/auth";
+import { generateArticle } from "../controllers/aiController";
+
+const aiRouter = express.Router();
+aiRouter.post("/generate-article", auth, generateArticle);
+
+export default aiRouter;
