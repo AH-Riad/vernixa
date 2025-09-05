3591c8fc8f9364f64869c2892dca02f38d73d49c
Riad <riadk910@gmail.com>
2025-09-05 19:05:47 +0600

    BlogTitle generator function added

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -19,7 +19,7 @@ export const generateArticle = async (req, res) => {
     const { prompt, length } = req.body;
 
     // Limit check for free users
-    if (plan !== "premium" && free_usage >= 15) {
+    if (plan !== "premium" && free_usage >= 50) {
       return res.json({
         success: false,
         message: "Limit reached. Upgrade to continue",
@@ -40,6 +40,57 @@ export const generateArticle = async (req, res) => {
     await sql`
       INSERT INTO creations(user_Id, prompt, content, type)
       VALUES(${userId}, ${prompt}, ${content}, 'article')
+    `;
+
+    // Update free usage if not premium
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    // Respond with generated article
+    res.json({ success: true, message: content });
+  } catch (error) {
+    console.error("generateArticle error:", error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//BlogTitles Generator
+export const generateBlogTitle = async (req, res) => {
+  try {
+    // Make sure auth middleware sets these
+    const userId = req.userId;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    const { prompt } = req.body;
+
+    // Limit check for free users
+    if (plan !== "premium" && free_usage >= 50) {
+      return res.json({
+        success: false,
+        message: "Limit reached. Upgrade to continue",
+      });
+    }
+
+    // Call OpenAI Gemini
+    const response = await openai.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0.7,
+      max_completion_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+
+    // Insert into Neon Postgres
+    await sql`
+      INSERT INTO creations(user_Id, prompt, content, type)
+      VALUES(${userId}, ${prompt}, ${content}, 'blog-title')
     `;
 
     // Update free usage if not premium
